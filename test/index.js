8e1a1b4942a464efcfecd21cf6407d575c8312fc
Lars-Magnus Skog <ralphtheninja@riseup.net>
2019-08-07 00:47:10 +0200

    fix standard :wrench:

--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@ const path = require('path')
 const exec = require('child_process').exec
 
 test('reative-path', t => {
-  const changed = exec(path.resolve(__dirname, '..', 'version-changed.js'), {cwd: __dirname}, () => {
-    const unchanged = exec(path.resolve(__dirname, '..', 'version-unchanged.js'), {cwd: __dirname}, () => {
+  const changed = exec(path.resolve(__dirname, '..', 'version-changed.js'), { cwd: __dirname }, () => {
+    const unchanged = exec(path.resolve(__dirname, '..', 'version-unchanged.js'), { cwd: __dirname }, () => {
       if (changed.exitCode === 1) {
         t.is(unchanged.exitCode, 0)
       } else if (changed.exitCode === 0) {
